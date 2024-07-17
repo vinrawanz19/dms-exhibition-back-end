f69b5d7a895731e6d9b38daa9133c9567866a315
vinrawanz19 <alvin.irawan@indomobil.co.id>
2024-07-17 11:07:19 +0700

    env database

--- src/database/database.py
+++ src/database/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/indomobil_dms_exhibition"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=10, max_overflow=20)
+engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URL"), pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
